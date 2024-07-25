d08fe5abe5479d0ed0cf50c3a4f1097e1e36d631
[Sara] <[saraperri3@icloud.com]>
2024-07-25 17:43:19 +0200

    First commit

---
+++ src/Optern2.java
@@ -0,0 +1,17 @@
+public class Optern2 {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+//        String result;
+//        if (value.length() >= 10) {
+//            result = "Lunghezza maggiore o uguale di 10";
+//        } else {
+//            result = "Lunghezza minore di 10";
+//        }
+        String result = value.length() >= 10 ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+
+        return result;
+    }
+}
